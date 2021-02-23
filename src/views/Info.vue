889a8ff47a42ceac40b66666b12afc8e2a141c73
Kevin Tran <kevintraan@live.com>
2021-02-23 10:14:52 -0700

    Changed resume link

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -45,7 +45,7 @@
     <v-row class="justify-center mt-5">
       <a
         class="text-h5"
-        href="https://drive.google.com/file/d/1EEY7xjSWxSZtEiJoYUdjAj-W9tU1hwBJ/view?usp=sharing"
+        href="https://drive.google.com/file/d/1SxqIwx0pUCBzEe81hAb32jJjDZvI6rv_/view?usp=sharing"
       >
         My Resume
       </a>
